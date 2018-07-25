cf6a9b931428433fe8a0648825669751d4c514fc
Zlatin Balevsky <zlatinb@gmail.com>
2018-07-25 14:21:13 +0100

    add dependencies on streaming lib, skeleton of i2p connector

--- core/src/main/groovy/com/muwire/core/connection/I2PConnector.groovy
+++ core/src/main/groovy/com/muwire/core/connection/I2PConnector.groovy
@@ -1,8 +1,17 @@
 package com.muwire.core.connection
 
+import net.i2p.client.streaming.I2PSocketManager
 import net.i2p.data.Destination
 
 class I2PConnector {
+	
+	final I2PSocketManager socketManager
+	
+	I2PConnector() {}
+	
+	I2PConnector(I2PSocketManager socketManager) {
+		this.socketManager = socketManager
+	}
 	
 	Endpoint connect(Destination dest) {
 		//TODO implement
