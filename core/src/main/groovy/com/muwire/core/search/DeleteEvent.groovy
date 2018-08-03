76b190b742de76cbc149f02d009f20bdf4b8c4d3
Zlatin Balevsky <zlatinb@gmail.com>
2018-08-03 22:47:15 +0100

    upsert and delete events

---
+++ core/src/main/groovy/com/muwire/core/search/DeleteEvent.groovy
@@ -0,0 +1,16 @@
+package com.muwire.core.search
+
+import com.muwire.core.Event
+
+import net.i2p.data.Base32
+import net.i2p.data.Destination
+
+class DeleteEvent extends Event {
+	byte [] infoHash
+	Destination leaf
+	
+	@Override
+	public String toString() {
+		"DeleteEvent ${super.toString()} infoHash:${Base32.encode(infoHash)} leaf:${leaf.toBase32()}"
+	}
+}
---
+++ core/src/main/groovy/com/muwire/core/search/UpsertEvent.groovy
@@ -0,0 +1,18 @@
+package com.muwire.core.search
+
+import com.muwire.core.Event
+
+import net.i2p.data.Base32
+import net.i2p.data.Destination
+
+class UpsertEvent extends Event {
+
+	List<String> names
+	byte [] infoHash
+	Destination leaf
+	
+	@Override
+	public String toString() {
+		"UpsertEvent ${super.toString()} names:$names infoHash:${Base32.encode(infoHash)} leaf:${leaf.toBase32()}"
+	}
+}
