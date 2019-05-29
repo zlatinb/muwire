4c9d3dc5f334c8de77e7d97f14bf4973ba22fa61
Zlatin Balevsky <zlatinb@gmail.com>
2019-05-29 17:12:31 +0100

    switch to Groovy syntax

--- gui/griffon-app/conf/Config.groovy
+++ gui/griffon-app/conf/Config.groovy
@@ -1,31 +1,14 @@
-import griffon.util.AbstractMapResourceBundle;
 
-import javax.annotation.Nonnull;
-import java.util.Map;
+application {
+    title = 'MuWire'
+    startupGroups = ['EventList']
+    autoShutdown = true
+}
 
-import static griffon.util.CollectionUtils.map;
-import static java.util.Collections.singletonList;
-
-public class Config extends AbstractMapResourceBundle {
-    @Override
-    protected void initialize(@Nonnull Map<String, Object> entries) {
-        map(entries)
-            .e("application", map()
-                .e("title", "MuWire")
-                .e("startupGroups", singletonList("container"))
-                .e("autoShutdown", true)
-            )
-            .e("mvcGroups", map()
-                .e("container", map()
-                    .e("model", "com.muwire.gui.EventListModel")
-                    .e("view", "com.muwire.gui.EventListView")
-                    .e("controller", "com.muwire.gui.EventListController")
-                )
-                .e("editor", map()
-                    .e("model", "com.muwire.gui.EventListModel")
-                    .e("view", "com.muwire.gui.EventListView")
-                    .e("controller", "com.muwire.gui.EventListController")
-                )
-            );
+mvcGroups {
+    'EventList' {
+        model = 'com.muwire.gui.EventListModel'
+        view = 'com.muwire.gui.EventListView'
+        controller = 'com.muwire.gui.EventListController'
     }
 }
