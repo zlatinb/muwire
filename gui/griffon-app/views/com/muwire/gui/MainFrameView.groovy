b58628c3917ea9cfc2def5565f22bb16ecbfe799
Zlatin Balevsky <zlatinb@gmail.com>
2019-05-30 15:37:43 +0100

    playing with layouts

--- gui/griffon-app/views/com/muwire/gui/MainFrameView.groovy
+++ gui/griffon-app/views/com/muwire/gui/MainFrameView.groovy
@@ -3,7 +3,19 @@ package com.muwire.gui
 import griffon.core.artifact.GriffonView
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
+
+import javax.swing.BorderFactory
+import javax.swing.Box
+import javax.swing.BoxLayout
 import javax.swing.SwingConstants
+import javax.swing.border.Border
+
+import java.awt.BorderLayout
+import java.awt.FlowLayout
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import java.awt.Insets
+
 import javax.annotation.Nonnull
 
 @ArtifactProviderFor(GriffonView)
@@ -15,17 +27,37 @@ class MainFrameView {
 
     void initUI() {
         builder.with {
-            application(size: [1024, 768], id: 'main-frame',
+            application(size : [1024,768], id: 'main-frame',
                 title: application.configuration['application.title'],
                 iconImage:   imageIcon('/griffon-icon-48x48.png').image,
                 iconImages: [imageIcon('/griffon-icon-48x48.png').image,
                              imageIcon('/griffon-icon-32x32.png').image,
-                             imageIcon('/griffon-icon-16x16.png').image],
+                             imageIcon('/griffon-icon-16x16.png').image], 
+                pack : false,
                 visible : bind { model.coreInitialized }) {
-                gridLayout(rows: 2, cols: 1)
-                label(id: 'clickLabel', text: bind { model.clickCount },
-                     horizontalAlignment: SwingConstants.CENTER)
-                button(id: 'clickButton', clickAction)
+                borderLayout()
+                panel (border: etchedBorder(), constraints : BorderLayout.NORTH) {
+                    borderLayout()
+                    panel (border: etchedBorder(), constraints: BorderLayout.WEST) {
+                        gridLayout(rows:1, cols: 2)
+                        button("1")
+                        button("2")
+                    }
+                    panel(constraints: BorderLayout.CENTER) {
+                        borderLayout()
+                        label("Enter search here:", constraints: BorderLayout.WEST)
+                        textField(constraints: BorderLayout.CENTER)
+                    }
+                    panel( border: etchedBorder(), constraints: BorderLayout.EAST) {
+                        button("Search")
+                    }
+                }
+                panel (border: etchedBorder(), constraints : BorderLayout.CENTER) {
+                    
+                }
+                panel (border: etchedBorder(), constraints : BorderLayout.SOUTH) {
+                    
+                }
             }
         }
     }
