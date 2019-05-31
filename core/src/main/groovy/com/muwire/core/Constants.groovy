5875e7d03f82d48d7dafa003b7d0e2439d5d8d88
Zlatin Balevsky <zlatinb@gmail.com>
2019-05-31 17:36:08 +0100

    change sig type

--- core/src/main/groovy/com/muwire/core/Constants.groovy
+++ core/src/main/groovy/com/muwire/core/Constants.groovy
@@ -4,7 +4,7 @@ import net.i2p.crypto.SigType
 
 class Constants {
     public static final byte PERSONA_VERSION = (byte)1
-    public static final SigType SIG_TYPE = SigType.ECDSA_SHA512_P521 // TODO: decide which
+    public static final SigType SIG_TYPE = SigType.EdDSA_SHA512_Ed25519 
     
     public static final int MAX_HEADER_SIZE = 0x1 << 14
     public static final int MAX_HEADERS = 16
