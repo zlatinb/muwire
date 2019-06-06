12283dba9d44d6f099da4b0584500d08fa77a9e2
Zlatin Balevsky <zlatinb@gmail.com>
2019-06-06 22:22:43 +0100

    Release 0.0.12 for search by hash

--- core/src/main/groovy/com/muwire/core/Core.groovy
+++ core/src/main/groovy/com/muwire/core/Core.groovy
@@ -248,7 +248,7 @@ public class Core {
             }
         }
         
-        Core core = new Core(props, home, "0.0.11")
+        Core core = new Core(props, home, "0.0.12")
         core.startServices()
         
         // ... at the end, sleep or execute script
