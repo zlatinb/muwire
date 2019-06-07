a5d442d3201dc5c4c52f5c45d4e7141e4383562b
Zlatin Balevsky <zlatinb@gmail.com>
2019-06-07 06:37:23 +0100

    Release 0.0.13 for keyword search fix

--- cli/src/main/groovy/com/muwire/cli/Cli.groovy
+++ cli/src/main/groovy/com/muwire/cli/Cli.groovy
@@ -34,7 +34,7 @@ class Cli {
         
         Core core 
         try {
-            core = new Core(props, home, "0.0.12")
+            core = new Core(props, home, "0.0.13")
         } catch (Exception bad) {
             bad.printStackTrace(System.out)
             println "Failed to initialize core, exiting"
