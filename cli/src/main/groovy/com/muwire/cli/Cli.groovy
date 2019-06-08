1299fb251276e95fd3eec654e41754cb2933671d
Zlatin Balevsky <zlatinb@gmail.com>
2019-06-08 08:04:35 +0100

    Release 0.1.1 for fixes and reduced piece size

--- cli/src/main/groovy/com/muwire/cli/Cli.groovy
+++ cli/src/main/groovy/com/muwire/cli/Cli.groovy
@@ -34,7 +34,7 @@ class Cli {
         
         Core core 
         try {
-            core = new Core(props, home, "0.1.0")
+            core = new Core(props, home, "0.1.1")
         } catch (Exception bad) {
             bad.printStackTrace(System.out)
             println "Failed to initialize core, exiting"
--- core/src/main/groovy/com/muwire/core/Core.groovy
+++ core/src/main/groovy/com/muwire/core/Core.groovy
@@ -253,7 +253,7 @@ public class Core {
             }
         }
         
-        Core core = new Core(props, home, "0.1.0")
+        Core core = new Core(props, home, "0.1.1")
         core.startServices()
         
         // ... at the end, sleep or execute script
