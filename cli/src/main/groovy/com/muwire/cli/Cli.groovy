a45e57f5ec99d1d0e83dd20883b63ff35ebd0be8
Zlatin Balevsky <zlatinb@gmail.com>
2019-06-13 10:28:44 +0100

    Release 0.1.7

--- cli/src/main/groovy/com/muwire/cli/Cli.groovy
+++ cli/src/main/groovy/com/muwire/cli/Cli.groovy
@@ -34,7 +34,7 @@ class Cli {
         
         Core core 
         try {
-            core = new Core(props, home, "0.1.6")
+            core = new Core(props, home, "0.1.7")
         } catch (Exception bad) {
             bad.printStackTrace(System.out)
             println "Failed to initialize core, exiting"
--- core/src/main/groovy/com/muwire/core/Core.groovy
+++ core/src/main/groovy/com/muwire/core/Core.groovy
@@ -260,7 +260,7 @@ public class Core {
             }
         }
         
-        Core core = new Core(props, home, "0.1.6")
+        Core core = new Core(props, home, "0.1.7")
         core.startServices()
         
         // ... at the end, sleep or execute script
