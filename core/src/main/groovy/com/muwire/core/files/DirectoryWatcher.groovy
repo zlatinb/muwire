1d6781819bd08512786e6bf933fecc5905d09091
Zlatin Balevsky <zlatinb@gmail.com>
2019-06-18 19:44:22 +0100

    ignore CWSE if shutting down

--- core/src/main/groovy/com/muwire/core/files/DirectoryWatcher.groovy
+++ core/src/main/groovy/com/muwire/core/files/DirectoryWatcher.groovy
@@ -5,6 +5,8 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.Paths
 import static java.nio.file.StandardWatchEventKinds.*
+
+import java.nio.file.ClosedWatchServiceException
 import java.nio.file.WatchEvent
 import java.nio.file.WatchKey
 import java.nio.file.WatchService
@@ -79,7 +81,7 @@ class DirectoryWatcher {
                 }
                 key.reset()
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException|ClosedWatchServiceException e) {
             if (!shutdown)
                 throw e
         }
