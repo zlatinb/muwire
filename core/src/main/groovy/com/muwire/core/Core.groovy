51425bbfd9429a34a9d2c679d1ca72c992a3af35
Zlatin Balevsky <zlatinb@gmail.com>
2019-06-24 07:38:39 +0100

    Release 0.3.8

--- core/src/main/groovy/com/muwire/core/Core.groovy
+++ core/src/main/groovy/com/muwire/core/Core.groovy
@@ -297,7 +297,7 @@ public class Core {
             }
         }
         
-        Core core = new Core(props, home, "0.3.7")
+        Core core = new Core(props, home, "0.3.8")
         core.startServices()
         
         // ... at the end, sleep or execute script
