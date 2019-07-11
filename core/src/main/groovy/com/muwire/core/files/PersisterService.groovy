56e9235d7b76dfb64027beb11ffa8a3f2d88a8ad
Zlatin Balevsky <zlatinb@gmail.com>
2019-07-11 15:28:25 +0100

    avoid FS call to get file length

--- core/src/main/groovy/com/muwire/core/files/PersisterService.groovy
+++ core/src/main/groovy/com/muwire/core/files/PersisterService.groovy
@@ -137,7 +137,7 @@ class PersisterService extends Service {
     private def toJson(File f, SharedFile sf) {
         def json = [:]
         json.file = Base64.encode DataUtil.encodei18nString(f.toString())
-        json.length = f.length()
+        json.length = sf.getCachedLength()
         InfoHash ih = sf.getInfoHash()
         json.infoHash = Base64.encode ih.getRoot()
         json.pieceSize = sf.getPieceSize()
