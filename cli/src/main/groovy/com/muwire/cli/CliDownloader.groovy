337605dc0fed8c3e277cd0db179a95d4e6b57195
Zlatin Balevsky <zlatinb@gmail.com>
2019-10-10 16:48:10 +0100

    Release 0.4.15

--- cli/src/main/groovy/com/muwire/cli/CliDownloader.groovy
+++ cli/src/main/groovy/com/muwire/cli/CliDownloader.groovy
@@ -53,7 +53,7 @@ class CliDownloader {
 
         Core core
         try {
-            core = new Core(props, home, "0.4.14")
+            core = new Core(props, home, "0.4.15")
         } catch (Exception bad) {
             bad.printStackTrace(System.out)
             println "Failed to initialize core, exiting"
--- core/src/main/groovy/com/muwire/core/Core.groovy
+++ core/src/main/groovy/com/muwire/core/Core.groovy
@@ -362,7 +362,7 @@ public class Core {
             }
         }
 
-        Core core = new Core(props, home, "0.4.14")
+        Core core = new Core(props, home, "0.4.15")
         core.startServices()
 
         // ... at the end, sleep or execute script
