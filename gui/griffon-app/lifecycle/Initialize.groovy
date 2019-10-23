1d5b12e2d70c52c9bab15610df784f8cd2d3080d
Zlatin Balevsky <zlatinb@gmail.com>
2019-10-23 06:31:08 +0100

    if core is not initialized, just shutdown

--- gui/griffon-app/lifecycle/Initialize.groovy
+++ gui/griffon-app/lifecycle/Initialize.groovy
@@ -69,7 +69,8 @@ class Initialize extends AbstractLifecycleHandler {
                             application.shutdown()
                         }as Runnable)
                         t.start()
-                    }
+                    } else
+                        application.shutdown()
                     tray.remove(trayIcon)
                 })
                 
