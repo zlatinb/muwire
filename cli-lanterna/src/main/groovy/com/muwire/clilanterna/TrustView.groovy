7bf0373b80c2cb4368f556de9b8d6d091cad3614
Zlatin Balevsky <zlatinb@gmail.com>
2019-10-25 08:24:07 +0100

    trust and distrust actions

--- cli-lanterna/src/main/groovy/com/muwire/clilanterna/TrustView.groovy
+++ cli-lanterna/src/main/groovy/com/muwire/clilanterna/TrustView.groovy
@@ -8,10 +8,15 @@ import com.googlecode.lanterna.gui2.LayoutData
 import com.googlecode.lanterna.gui2.Panel
 import com.googlecode.lanterna.gui2.TextGUI
 import com.googlecode.lanterna.gui2.Window
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton
 import com.googlecode.lanterna.gui2.GridLayout.Alignment
 import com.googlecode.lanterna.gui2.Label
 import com.googlecode.lanterna.gui2.table.Table
 import com.muwire.core.Core
+import com.muwire.core.Persona
+import com.muwire.core.trust.TrustEvent
+import com.muwire.core.trust.TrustLevel
 
 class TrustView extends BasicWindow {
     private final TrustModel model
@@ -74,11 +79,65 @@ class TrustView extends BasicWindow {
     }
     
     private void trustedActions() {
+        int selectedRow = trusted.getSelectedRow()
+        def row = model.modelTrusted.getRow(selectedRow)
+        Persona persona = row[0].persona
         
+        Window prompt = new BasicWindow("Change trust for "+persona.getHumanReadableName())
+        prompt.setHints([Window.Hint.CENTERED])
+        Panel contentPanel = new Panel()
+        contentPanel.setLayoutManager(new GridLayout(3))
+        LayoutData layoutData = GridLayout.createLayoutData(Alignment.CENTER, Alignment.CENTER)
+        
+        Button markNeutral = new Button("Mark Neutral", {
+            core.eventBus.publish(new TrustEvent(persona : persona, level : TrustLevel.NEUTRAL))
+            MessageDialog.showMessageDialog(textGUI, "Marked Neutral", persona.getHumanReadableName() + "has been marked neutral",
+                 MessageDialogButton.OK)  
+        })
+        Button markDistrusted = new Button("Mark Distrusted", {
+            core.eventBus.publish(new TrustEvent(persona : persona, level : TrustLevel.DISTRUSTED))
+            MessageDialog.showMessageDialog(textGUI, "Marked Distrusted", persona.getHumanReadableName() + "has been marked distrusted",
+                 MessageDialogButton.OK)
+        })
+        Button closeButton = new Button("Close", {prompt.close()})
+        contentPanel.with { 
+            addComponent(markNeutral, layoutData)
+            addComponent(markDistrusted, layoutData)
+            addComponent(closeButton, layoutData)
+        }
+        prompt.setComponent(contentPanel)
+        textGUI.addWindowAndWait(prompt)
     }
     
     private void distrustedActions() {
+        int selectedRow = trusted.getSelectedRow()
+        def row = model.modelDistrusted.getRow(selectedRow)
+        Persona persona = row[0].persona
         
+        Window prompt = new BasicWindow("Change trust for "+persona.getHumanReadableName())
+        prompt.setHints([Window.Hint.CENTERED])
+        Panel contentPanel = new Panel()
+        contentPanel.setLayoutManager(new GridLayout(3))
+        LayoutData layoutData = GridLayout.createLayoutData(Alignment.CENTER, Alignment.CENTER)
+        
+        Button markNeutral = new Button("Mark Neutral", {
+            core.eventBus.publish(new TrustEvent(persona : persona, level : TrustLevel.NEUTRAL))
+            MessageDialog.showMessageDialog(textGUI, "Marked Neutral", persona.getHumanReadableName() + "has been marked neutral",
+                 MessageDialogButton.OK)
+        })
+        Button markDistrusted = new Button("Mark Trusted", {
+            core.eventBus.publish(new TrustEvent(persona : persona, level : TrustLevel.TRUSTED))
+            MessageDialog.showMessageDialog(textGUI, "Marked Trusted", persona.getHumanReadableName() + "has been marked trusted",
+                 MessageDialogButton.OK)
+        })
+        Button closeButton = new Button("Close", {prompt.close()})
+        contentPanel.with {
+            addComponent(markDistrusted, layoutData)
+            addComponent(markNeutral, layoutData)
+            addComponent(closeButton, layoutData)
+        }
+        prompt.setComponent(contentPanel)
+        textGUI.addWindowAndWait(prompt)
     }
     
     private void trustListActions() {
