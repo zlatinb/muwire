29562c42ea129306100a6292f70a566490abeb68
Zlatin Balevsky <zlatinb@gmail.com>
2019-10-27 05:12:01 +0000

    add toString()

--- core/src/main/groovy/com/muwire/core/files/DirectoryUnsharedEvent.groovy
+++ core/src/main/groovy/com/muwire/core/files/DirectoryUnsharedEvent.groovy
@@ -4,4 +4,8 @@ import com.muwire.core.Event
 
 class DirectoryUnsharedEvent extends Event {
     File directory
+    
+    public String toString() {
+        super.toString() + " unshared directory "+ directory.toString()
+    }
 }
