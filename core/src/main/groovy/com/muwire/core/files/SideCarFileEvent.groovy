00c529cca11eedb09e38237945391e7f7d9036f4
Zlatin Balevsky <zlatinb@gmail.com>
2019-11-02 00:40:08 +0000

    toString()

--- core/src/main/groovy/com/muwire/core/files/SideCarFileEvent.groovy
+++ core/src/main/groovy/com/muwire/core/files/SideCarFileEvent.groovy
@@ -4,4 +4,9 @@ import com.muwire.core.Event
 
 class SideCarFileEvent extends Event {
     File file
+    
+    @Override
+    public String toString() {
+        return super.toString() + " file: "+file.getAbsolutePath()
+    }
 }
