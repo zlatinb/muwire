a2637570b1c23298eed406aca78353990aaf753b
Zlatin Balevsky <zlatinb@gmail.com>
2019-11-10 06:23:28 +0000

    Release 0.6.1

--- cli-lanterna/src/main/groovy/com/muwire/clilanterna/CliLanterna.groovy
+++ cli-lanterna/src/main/groovy/com/muwire/clilanterna/CliLanterna.groovy
@@ -32,7 +32,7 @@ import com.muwire.core.UILoadedEvent
 import com.muwire.core.files.AllFilesLoadedEvent
 
 class CliLanterna {
-    private static final String MW_VERSION = "0.6.0"
+    private static final String MW_VERSION = "0.6.1"
     
     private static volatile Core core
     
