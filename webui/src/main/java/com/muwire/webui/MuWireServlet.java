efef0f3734e37eb890e697bffccae7103aec0ae0
Zlatin Balevsky <zlatinb@gmail.com>
2019-11-29 18:00:32 +0000

    include a servlet as well as pre-compiled jsps

---
+++ webui/src/main/java/com/muwire/webui/MuWireServlet.java
@@ -0,0 +1,14 @@
+package com.muwire.webui;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class MuWireServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+}
