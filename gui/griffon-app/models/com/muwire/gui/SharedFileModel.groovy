9337d1b74d116a1d2b8fcfdf01d6af430c219195
Zlatin Balevsky <zlatinb@gmail.com>
2020-02-14 01:48:02 +0000

    chase down references to missing infoHash #35

--- gui/griffon-app/models/com/muwire/gui/SharedFileModel.groovy
+++ gui/griffon-app/models/com/muwire/gui/SharedFileModel.groovy
@@ -1,6 +1,7 @@
 package com.muwire.gui
 
 import com.muwire.core.Core
+import com.muwire.core.InfoHash
 import com.muwire.core.SharedFile
 
 import griffon.core.artifact.GriffonModel
@@ -21,6 +22,6 @@ class SharedFileModel {
     public void mvcGroupInit(Map<String,String> args) {
         searchers.addAll(sf.getSearches())
         downloaders.addAll(sf.getDownloaders())
-        certificates.addAll(core.certificateManager.byInfoHash.getOrDefault(sf.infoHash,[]))
+        certificates.addAll(core.certificateManager.byInfoHash.getOrDefault(new InfoHash(sf.getRoot()),[]))
     }
 }
