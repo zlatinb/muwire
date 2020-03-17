244015465afcd86961b232b07904256b2b20b88a
Zlatin Balevsky <zlatinb@gmail.com>
2020-03-17 16:31:29 +0000

    avoid groovy's implicit conversion to BigDecimal

--- gui/src/main/groovy/com/muwire/gui/DownloaderComparator.groovy
+++ gui/src/main/groovy/com/muwire/gui/DownloaderComparator.groovy
@@ -6,8 +6,8 @@ class DownloaderComparator implements Comparator<Downloader>{
 
     @Override
     public int compare(Downloader o1, Downloader o2) {
-        double d1 = o1.donePieces() * 1.0 / o1.nPieces
-        double d2 = o2.donePieces() * 1.0 / o2.nPieces
+        double d1 = o1.donePieces().toDouble() / o1.nPieces
+        double d2 = o2.donePieces().toDouble() / o2.nPieces
         return Double.compare(d1, d2);
     }
 }
