034e706d17f6fb475480bee09d91840bdedb3d43
Zlatin Balevsky <zlatinb@gmail.com>
2020-10-05 07:03:18 +0100

    Release 0.7.5

--- core/src/main/groovy/com/muwire/core/Core.groovy
+++ core/src/main/groovy/com/muwire/core/Core.groovy
@@ -561,7 +561,7 @@ public class Core {
             }
         }
 
-        Core core = new Core(props, home, "0.7.4")
+        Core core = new Core(props, home, "0.7.5")
         core.startServices()
 
         // ... at the end, sleep or execute script
