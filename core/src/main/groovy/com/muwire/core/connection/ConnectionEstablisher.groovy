1ed74768fb0f34f772879ebc1e3461e5fa70a5b1
Zlatin Balevsky <zlatinb@gmail.com>
2020-10-18 19:48:45 +0100

    fix assignment

--- core/src/main/groovy/com/muwire/core/connection/ConnectionEstablisher.groovy
+++ core/src/main/groovy/com/muwire/core/connection/ConnectionEstablisher.groovy
@@ -80,7 +80,7 @@ class ConnectionEstablisher {
         def toTry = hostCache.getHosts(1, {!connectionManager.isConnected(it) && !inProgress.contains(it)} as Predicate)
         if (toTry.isEmpty())
             return
-        toTry == toTry[0]
+        toTry = toTry[0]
         if (!connectionManager.isConnected(toTry) && inProgress.add(toTry))
             executor.execute({connect(toTry)} as Runnable)
     }
