cad3946d3e415e412352fba41a657f6ebd890da4
Zlatin Balevsky <zlatinb@gmail.com>
2020-11-01 07:02:05 +0000

    fix event

--- core/src/main/groovy/com/muwire/core/collections/CollectionFetchedEvent.groovy
+++ core/src/main/groovy/com/muwire/core/collections/CollectionFetchedEvent.groovy
@@ -1,8 +1,9 @@
 package com.muwire.core.collections
 
+import com.muwire.core.Event
 import com.muwire.core.InfoHash
 
-class CollectionFetchedEvent {
+class CollectionFetchedEvent extends Event {
     FileCollection collection
     UUID uuid
     InfoHash infoHash
