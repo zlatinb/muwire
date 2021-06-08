fd06fdf7ce9ca586a846c6158244cfe6727178be
Zlatin Balevsky <zlatinb@gmail.com>
2021-06-08 22:30:33 +0100

    spltt search index into Java and Groovy parts for faster initial loading.  GitHub issue #59

--- core/src/main/groovy/com/muwire/core/search/SearchIndex.groovy
+++ core/src/main/groovy/com/muwire/core/search/SearchIndex.groovy
@@ -1,111 +1,28 @@
 package com.muwire.core.search
 
 import com.muwire.core.SplitPattern
-import net.metanotionz.io.Serializer
-import net.metanotionz.io.block.BlockFile
-import net.metanotionz.util.skiplist.SkipList
 
-import java.nio.charset.StandardCharsets
-
+/**
+ * This is split into a Groovy part and Java part.
+ * The Groovy part is needed because Java regexes do not support
+ * the full split string.
+ * The Java part is needed for speed.
+ */
 class SearchIndex {
 
-    private final SkipList keywords, hashes
+    
+    private final SearchIndexImpl actualSearchIndex
     
     SearchIndex(String name) {
-        BlockFile blockFile = new BlockFile(name, true)
-        keywords = blockFile.makeIndex("keywords", new StringSerializer(), new HashArraySerializer())
-        hashes = blockFile.makeIndex("hashes", new HashSerializer(), new StringArraySerializer())
+        actualSearchIndex = new SearchIndexImpl(name)
     }
     
     void add(String string) {
-        final int hash = string.hashCode()
-        String [] split = split(string)
-        split.each {keyword ->
-            int [] existingHashes = (int[])keywords.get(keyword)
-            if (existingHashes == null) {
-                existingHashes = new int[1]
-                existingHashes[0] = hash
-                keywords.put(keyword, existingHashes)
-                hashes.put(hash, new String[] {string})
-            } else {
-                boolean found = false
-                for (int existingHash : existingHashes) {
-                    if (existingHash == hash) {
-                        found = true
-                        break
-                    }
-                }
-                if (!found) {
-                    int [] newHashes = new int[existingHashes.length + 1]
-                    System.arraycopy(existingHashes, 0, newHashes, 0, existingHashes.length)
-                    newHashes[newHashes.length - 1] = hash
-                    keywords.put(keyword, newHashes)
-                }
-                
-                String[] fileNames = (String []) hashes.get(hash)
-                if (fileNames == null) {
-                    fileNames = new String[] {string}
-                    hashes.put(hash, fileNames)
-                } else {
-                    Set<String> unique = new HashSet<>()
-                    for (String fileName : fileNames)
-                        unique.add(fileName)
-                    unique.add(string)
-                    hashes.put(hash, unique.toArray(new String[0]))
-                }
-            }
-        }
+        actualSearchIndex.add(string, split(string))
     }
 
     void remove(String string) {
-        final int hash = string.hashCode()
-        
-        // check if such string exists at all
-        String [] strings = (String[]) hashes.get(hash)
-        if (strings == null)
-            return
-        
-        int idx = -1
-        for (int i = 0; i < strings.length; i++) {
-            if (strings[i] == string) {
-                idx = i
-                break
-            }
-        }
-        
-        if (idx == -1)
-            return
-        
-        String [] newStrings = new String[strings.length - 1]
-        System.arraycopy(strings, 0, newStrings, 0, idx)
-        System.arraycopy(strings, idx + 1, newStrings, idx, newStrings.length - idx)
-        
-        hashes.put(hash, newStrings)
-        
-        String [] split = split(string)
-        for (String keyword : split) {
-            int [] existingHashes = (int [])keywords.get(keyword)
-            if (existingHashes == null)
-                throw new IllegalStateException()
-            idx = -1
-            for (int i = 0; i < existingHashes.length; i ++) {
-                if (existingHashes[i] == hash) {
-                    idx = i
-                    break
-                }
-            }
-            if (idx == -1)
-                throw new IllegalStateException()
-            
-            if (existingHashes.length == 1) {
-                keywords.remove(keyword)
-            } else {
-                int [] newHashes = new int[existingHashes.length - 1]
-                System.arraycopy(existingHashes, 0, newHashes, 0, idx)
-                System.arraycopy(existingHashes, idx + 1, newHashes, idx, newHashes.length - idx)
-                keywords.put(keyword, newHashes)
-            }
-        }
+        actualSearchIndex.remove(string, split(string))
     }
 
     private static String[] split(final String source) {
@@ -125,154 +42,8 @@ class SearchIndex {
     }
 
     String[] search(List<String> terms) {
-        Set<String> rv = null;
-
-        Set<String> powerSet = new HashSet<>()
-        terms.each {
-            powerSet.addAll(it.toLowerCase().split(' '))
-        }
-        
-        powerSet.each {
-            Set<String> forWord = new HashSet<>()
-            int [] wordHashes = (int[])keywords.get(it)
-            for (int h : wordHashes) {
-                String [] forHashArray = hashes.get(h)
-                if (forHashArray == null)
-                    continue
-                for (String s : forHashArray)
-                    forWord.add(s)
-            }
-            if (rv == null) {
-                rv = new HashSet<>(forWord)
-            } else {
-                rv.retainAll(forWord)
-            }
-        }
-        
-        // now, filter by terms
-        for (Iterator<String> iter = rv.iterator(); iter.hasNext();) {
-            String candidate = iter.next()
-            candidate = candidate.toLowerCase()
-            boolean keep = true
-            terms.each { 
-                keep &= candidate.contains(it)
-            }
-            if (!keep)
-                iter.remove()
-        }
-
-        if (rv != null)
-            return rv.asList()
-        []
+        return actualSearchIndex.search(terms)
     }
     
-    private static class HashSerializer implements Serializer {
 
-        @Override
-        byte[] getBytes(Object o) {
-            if (!(o instanceof Integer))
-                throw new IllegalArgumentException()
-            int hashCode = (Integer)o
-            byte [] hash = new byte[4]
-            hash [0] = (byte)((hashCode >>> 24) & 0xFF)
-            hash [1] = (byte)((hashCode >>> 16) & 0xFF)
-            hash [2] = (byte)((hashCode >>> 8) & 0xFF)
-            hash [3] = (byte) (hashCode & 0xFF)
-            return hash
-        }
-
-        @Override
-        Object construct(byte[] b) {
-            int rv = (b[0] & 0xFF) << 24 |
-                    (b[1] & 0xFF) << 16 |
-                    (b[2] & 0xFF) << 8 |
-                    (b[3] & 0xFF)
-            rv
-        }
-    }
-    
-    private static class HashArraySerializer implements Serializer {
-
-        @Override
-        byte[] getBytes(Object o) {
-            if (!(o instanceof int[]))
-                throw new IllegalArgumentException()
-            int [] hashes = (int[]) o
-            byte [] rv = new byte[hashes.length * 4]
-            for (int i = 0; i < hashes.length; i ++) {
-                int hash = hashes[i]
-                rv[i * 4] = (byte) ((hash >>> 24) & 0xFF)
-                rv[i * 4 + 1] = (byte) ((hash >>> 16) & 0xFF)
-                rv[i * 4 + 2] = (byte) ((hash >>> 8) & 0xFF)
-                rv[i * 4 + 3] = (byte) (hash & 0xFF)
-            }
-            return rv
-        }
-
-        @Override
-        Object construct(byte[] b) {
-            if (b.length % 4 != 0)
-                throw new IllegalArgumentException("invalid length ${b.length}")
-            int [] rv = new int[b.length / 4]
-            for (int i = 0; i < rv.length; i ++) {
-                rv[i] = (b[i * 4] & 0xFF) << 24 |
-                        (b[i * 4 + 1] & 0xFF) << 16 |
-                        (b[i * 4 + 2] & 0xFF) << 8 |
-                        (b[i * 4 + 3] & 0xFF)
-            }
-            rv
-        }
-    }
-    
-    private static class StringSerializer implements Serializer {
-
-        @Override
-        byte[] getBytes(Object o) {
-            if (!(o instanceof String))
-                throw new IllegalArgumentException()
-            String s = (String)o
-            return s.getBytes(StandardCharsets.UTF_8)
-        }
-
-        @Override
-        Object construct(byte[] b) {
-            new String(b, StandardCharsets.UTF_8)
-        }
-    }
-    
-    private static class StringArraySerializer implements Serializer {
-
-        @Override
-        byte[] getBytes(Object o) {
-            if (!(o instanceof String[]))
-                throw new IllegalArgumentException()
-            String[] strings = (String[]) o
-            def baos = new ByteArrayOutputStream()
-            def daos = new DataOutputStream(baos)
-            daos.writeInt(strings.length)
-            strings.each {
-                byte [] utf8 = it.getBytes(StandardCharsets.UTF_8)
-                daos.writeInt(utf8.length)
-                daos.write(utf8)
-            }
-            daos.close()
-            baos.toByteArray()
-        }
-
-        @Override
-        Object construct(byte[] b) {
-            def dis = new DataInputStream(new ByteArrayInputStream(b))
-            int count = dis.readInt()
-            if (count < 0)
-                throw new IllegalStateException("negative count $count")
-            String[] rv = new String[count]
-            for (int i = 0; i < count; i ++) {
-                int length = dis.readInt()
-                byte [] tmp = new byte[length]
-                dis.readFully(tmp)
-                rv[i] = new String(tmp, StandardCharsets.UTF_8)
-            }
-            rv
-        }
-    }
 }
---
+++ core/src/main/java/com/muwire/core/search/SearchIndexImpl.java
@@ -0,0 +1,264 @@
+package com.muwire.core.search;
+
+import net.metanotionz.io.Serializer;
+import net.metanotionz.io.block.BlockFile;
+import net.metanotionz.util.skiplist.SkipList;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class SearchIndexImpl {
+    
+    private final SkipList<String, int[]> keywords;
+    private final SkipList<Integer, String[]> hashes;
+    
+    SearchIndexImpl(String name) throws IOException {
+        BlockFile blockFile = new BlockFile(name, true);
+        keywords = blockFile.makeIndex("keywords", new StringSerializer(), new HashArraySerializer());
+        hashes = blockFile.makeIndex("hashes", new HashSerializer(), new StringArraySerializer());
+    }
+
+    
+    void add(String string, String [] split) throws IOException {
+        final int hash = string.hashCode();
+        for (String keyword : split) {
+            int [] existingHashes = keywords.get(keyword);
+            if (existingHashes == null) {
+                existingHashes = new int[1];
+                existingHashes[0] = hash;
+                keywords.put(keyword, existingHashes);
+                hashes.put(hash, new String[] {string});
+            } else {
+                boolean found = false;
+                for (int existingHash : existingHashes) {
+                    if (existingHash == hash) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    int [] newHashes = new int[existingHashes.length + 1];
+                    System.arraycopy(existingHashes, 0, newHashes, 0, existingHashes.length);
+                    newHashes[newHashes.length - 1] = hash;
+                    keywords.put(keyword, newHashes);
+                }
+
+                String[] fileNames = hashes.get(hash);
+                if (fileNames == null) {
+                    fileNames = new String[] {string};
+                    hashes.put(hash, fileNames);
+                } else {
+                    Set<String> unique = new HashSet<>();
+                    for (String fileName : fileNames)
+                        unique.add(fileName);
+                    unique.add(string);
+                    hashes.put(hash, unique.toArray(new String[0]));
+                }
+            }
+        }
+    }
+    
+    void remove(String string, String[] split) throws IOException {
+        final int hash = string.hashCode();
+
+        // check if such string exists at all
+        String [] strings = hashes.get(hash);
+        if (strings == null)
+            return;
+
+        int idx = -1;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].equals(string)) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1)
+            return;
+
+        String [] newStrings = new String[strings.length - 1];
+        System.arraycopy(strings, 0, newStrings, 0, idx);
+        System.arraycopy(strings, idx + 1, newStrings, idx, newStrings.length - idx);
+
+        hashes.put(hash, newStrings);
+
+        for (String keyword : split) {
+            int [] existingHashes = keywords.get(keyword);
+            if (existingHashes == null)
+                throw new IllegalStateException();
+            idx = -1;
+            for (int i = 0; i < existingHashes.length; i ++) {
+                if (existingHashes[i] == hash) {
+                    idx = i;
+                    break;
+                }
+            }
+            if (idx == -1)
+                throw new IllegalStateException();
+
+            if (existingHashes.length == 1) {
+                keywords.remove(keyword);
+            } else {
+                int [] newHashes = new int[existingHashes.length - 1];
+                System.arraycopy(existingHashes, 0, newHashes, 0, idx);
+                System.arraycopy(existingHashes, idx + 1, newHashes, idx, newHashes.length - idx);
+                keywords.put(keyword, newHashes);
+            }
+        }
+    }
+    
+    String [] search (List<String> terms) throws IOException {
+        Set<String> rv = null;
+
+        Set<String> powerSet = new HashSet<>();
+        for(String it : terms) {
+            String [] split = it.toLowerCase().split(" ");
+            for (String splitIt : split)
+                powerSet.add(splitIt);
+        }
+
+        for (String it : powerSet) {
+            Set<String> forWord = new HashSet<>();
+            int [] wordHashes = keywords.get(it);
+            if (wordHashes == null)
+                continue;
+            for (int h : wordHashes) {
+                String [] forHashArray = hashes.get(h);
+                if (forHashArray == null)
+                    continue;
+                for (String s : forHashArray)
+                    forWord.add(s);
+            }
+            if (rv == null) {
+                rv = new HashSet<>(forWord);
+            } else {
+                rv.retainAll(forWord);
+            }
+        }
+
+        if (rv == null) {
+            return new String[0];
+        }
+        
+        // now, filter by terms
+        for (Iterator<String> iter = rv.iterator(); iter.hasNext();) {
+            String candidate = iter.next();
+            candidate = candidate.toLowerCase();
+            boolean keep = true;
+            for (String it : terms) {
+                keep &= candidate.contains(it);
+            }
+            if (!keep)
+                iter.remove();
+        }
+        return rv.toArray(new String[0]);
+    }
+    
+    private static class HashSerializer implements Serializer<Integer> {
+
+        @Override
+        public byte[] getBytes(Integer hashCode) {
+            byte [] hash = new byte[4];
+            hash [0] = (byte)((hashCode >>> 24) & 0xFF);
+            hash [1] = (byte)((hashCode >>> 16) & 0xFF);
+            hash [2] = (byte)((hashCode >>> 8) & 0xFF);
+            hash [3] = (byte) (hashCode & 0xFF);
+            return hash;
+        }
+
+        @Override
+        public Integer construct(byte[] b) {
+            int rv = (b[0] & 0xFF) << 24 |
+                    (b[1] & 0xFF) << 16 |
+                    (b[2] & 0xFF) << 8 |
+                    (b[3] & 0xFF);
+            return rv;
+        }
+    }
+
+    private static class HashArraySerializer implements Serializer<int[]> {
+
+        @Override
+        public byte[] getBytes(int[] hashes) {
+            byte [] rv = new byte[hashes.length * 4];
+            for (int i = 0; i < hashes.length; i ++) {
+                int hash = hashes[i];
+                rv[i * 4] = (byte) ((hash >>> 24) & 0xFF);
+                rv[i * 4 + 1] = (byte) ((hash >>> 16) & 0xFF);
+                rv[i * 4 + 2] = (byte) ((hash >>> 8) & 0xFF);
+                rv[i * 4 + 3] = (byte) (hash & 0xFF);
+            }
+            return rv;
+        }
+
+        @Override
+        public int[] construct(byte[] b) {
+            if (b.length % 4 != 0)
+                throw new IllegalArgumentException("invalid length ${b.length}");
+            int [] rv = new int[b.length / 4];
+            for (int i = 0; i < rv.length; i ++) {
+                rv[i] = (b[i * 4] & 0xFF) << 24 |
+                        (b[i * 4 + 1] & 0xFF) << 16 |
+                        (b[i * 4 + 2] & 0xFF) << 8 |
+                        (b[i * 4 + 3] & 0xFF);
+            }
+            return rv;
+        }
+    }
+
+    private static class StringSerializer implements Serializer<String> {
+
+        @Override
+        public byte[] getBytes(String s) {
+            return s.getBytes(StandardCharsets.UTF_8);
+        }
+
+        @Override
+        public String construct(byte[] b) {
+            return new String(b, StandardCharsets.UTF_8);
+        }
+    }
+
+    private static class StringArraySerializer implements Serializer<String[]> {
+
+        @Override
+        public byte[] getBytes(String[] strings) {
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                DataOutputStream daos = new DataOutputStream(baos);
+                daos.writeInt(strings.length);
+                for (String it : strings) {
+                    byte[] utf8 = it.getBytes(StandardCharsets.UTF_8);
+                    daos.writeInt(utf8.length);
+                    daos.write(utf8);
+                }
+                daos.close();
+                return baos.toByteArray();
+            } catch (IOException impossible) {
+                throw new RuntimeException(impossible);
+            }
+        }
+
+        @Override
+        public String[] construct(byte[] b) {
+            try {
+                DataInputStream dis = new DataInputStream(new ByteArrayInputStream(b));
+                int count = dis.readInt();
+                if (count < 0)
+                    throw new IllegalStateException("negative count $count");
+                String[] rv = new String[count];
+                for (int i = 0; i < count; i++) {
+                    int length = dis.readInt();
+                    byte[] tmp = new byte[length];
+                    dis.readFully(tmp);
+                    rv[i] = new String(tmp, StandardCharsets.UTF_8);
+                }
+                return rv;
+            } catch (IOException impossible) {
+                throw new RuntimeException(impossible);
+            }
+        }
+    }
+}
