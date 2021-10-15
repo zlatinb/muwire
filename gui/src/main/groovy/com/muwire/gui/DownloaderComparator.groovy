33b5ca184c43af98cf46c8edff72294f402a2acb
Zlatin Balevsky <zlatinb@gmail.com>
2021-10-15 12:04:09 +0100

    fix downloader progress comparator, GitHub issue #89

--- gui/src/main/groovy/com/muwire/gui/DownloaderComparator.groovy
+++ gui/src/main/groovy/com/muwire/gui/DownloaderComparator.groovy
@@ -6,8 +6,8 @@ class DownloaderComparator implements Comparator<Downloader>{
 
     @Override
     public int compare(Downloader o1, Downloader o2) {
-        double d1 = o1.donePieces().toDouble() / o1.nPieces
-        double d2 = o2.donePieces().toDouble() / o2.nPieces
+        double d1 = o1.donePieces().toDouble() / o1.getNPieces()
+        double d2 = o2.donePieces().toDouble() / o2.getNPieces()
         return Double.compare(d1, d2);
     }
 }
