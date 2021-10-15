a1312636863ef81cc203e2fc584b44e9d893b53c
Zlatin Balevsky <zlatinb@gmail.com>
2021-10-15 17:29:01 +0100

    limit the simultaneous connections to a given destination to 4.  GitHub issue #90

--- core/src/main/groovy/com/muwire/core/connection/I2PConnector.groovy
+++ core/src/main/groovy/com/muwire/core/connection/I2PConnector.groovy
@@ -3,9 +3,16 @@ package com.muwire.core.connection
 import net.i2p.client.streaming.I2PSocketManager
 import net.i2p.data.Destination
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Semaphore
+
 class I2PConnector {
 
+    private static final int PERMITS = 4
+    
     final I2PSocketManager socketManager
+    
+    private final Map<Destination, Semaphore> limiter = new ConcurrentHashMap<>()
 
     I2PConnector() {}
 
@@ -14,8 +21,14 @@ class I2PConnector {
     }
 
     Endpoint connect(Destination dest) {
-        def socket = socketManager.connect(dest)
-        new Endpoint(dest, socket.getInputStream(), socket.getOutputStream(), socket)
+        Semaphore limit = limiter.computeIfAbsent(dest, {new Semaphore(PERMITS)})
+        limit.acquire()
+        try {
+            def socket = socketManager.connect(dest)
+            return new Endpoint(dest, socket.getInputStream(), socket.getOutputStream(), socket)
+        } finally {
+            limit.release()
+        }
     }
 
 }
