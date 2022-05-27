d3dcbba34c2a03ae31ecd3d72c8a99baec5359eb
Zlatin Balevsky <zlatinb@gmail.com>
2022-05-27 21:36:30 +0100

    Use Collator for sorting Personas, GitHub issue #140

--- gui/src/main/groovy/com/muwire/gui/PersonaComparator.groovy
+++ gui/src/main/groovy/com/muwire/gui/PersonaComparator.groovy
@@ -2,9 +2,11 @@ package com.muwire.gui
 
 import com.muwire.core.Persona
 
+import java.text.Collator
+
 class PersonaComparator implements Comparator<Persona>{
     @Override
     int compare(Persona a, Persona b) {
-        return String.compare(a.getHumanReadableName(), b.getHumanReadableName())
+        return Collator.getInstance().compare(a.getHumanReadableName(), b.getHumanReadableName())
     }
 }
