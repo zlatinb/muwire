de88d829365c773fa74a36160bb1d240ba5b3dbb
Zlatin Balevsky <zlatinb@gmail.com>
2022-08-14 07:08:52 +0100

    do not use cached path in name comparator to avoid mass creation of VisualCache objects

--- gui/src/main/groovy/com/muwire/gui/SharedFileNameComparator.groovy
+++ gui/src/main/groovy/com/muwire/gui/SharedFileNameComparator.groovy
@@ -8,8 +8,8 @@ class SharedFileNameComparator implements Comparator<SharedFile>{
 
     @Override
     int compare(SharedFile o1, SharedFile o2) {
-        String path1 = o1.getCachedPath()
-        String path2 = o2.getCachedPath()
+        String path1 = o1.getFile().getAbsolutePath()
+        String path2 = o2.getFile().getAbsolutePath()
         return Collator.getInstance().compare(path1, path2)
     }
 }
