a2c84b7714866d9e2dd70760be58e000a5e6d614
Zlatin Balevsky <zlatinb@gmail.com>
2022-08-25 22:18:22 +0100

    wip on dbus notifications

--- gui/src/main/groovy/com/muwire/gui/linux/NotifyService.groovy
+++ gui/src/main/groovy/com/muwire/gui/linux/NotifyService.groovy
@@ -6,6 +6,7 @@ import org.freedesktop.dbus.connections.impl.DBusConnectionBuilder
 import org.freedesktop.dbus.interfaces.DBusInterface
 import org.freedesktop.dbus.types.UInt32
 import org.freedesktop.dbus.types.UInt64
+import org.freedesktop.dbus.types.Variant
 
 class NotifyService {
     
@@ -20,16 +21,20 @@ class NotifyService {
     static void notify(String text) {
         println "notifying $text"
         Notifications notifications = CONNECTION.getRemoteObject(BUS_NAME, BUS_PATH, Notifications.class)
-        println "capabilities"
-        println notifications.GetCapabilities()
+        boolean sound = notifications.GetCapabilities().contains("sound")
         
+        Map<String, Variant<?>> hints = new HashMap<>()
+        if (sound) {
+            Variant<String> variant = new Variant("message-new-instant")
+            hints.put("sound-name", variant)
+        }
         UInt32 rv = notifications.Notify("MuWire", // app name 
                 new UInt32(0L), // replaces
                 "", // no icon
                 text, // summary
                 text, // body
                 Collections.emptyList(), // actions
-                Collections.emptyMap(), // hints
+                hints, // hints
                 -1) // expire timeout
         
         println "rv is $rv"
